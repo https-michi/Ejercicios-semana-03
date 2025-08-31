9c1effa38355e29f4f18466e3220a0b8014ce228
https-michi <glitchmichi@gmail.com>
2025-08-30 20:47:18 -0500

    chore(project): inicializar proyecto Java con ejercicios de la semana 03

---
+++ ejercicios-semana-03/src/main/java/com/ejercicios/semana03/EjerciciosSemana03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ejercicios.semana03;
+
+/**
+ *
+ * @author snowden
+ */
+public class EjerciciosSemana03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
